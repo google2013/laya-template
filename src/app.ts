f1f98ed463a696ffeb8e70aa6c8c8d65f76556f1
pawanake <382729098@qq.com>
2016-05-03 18:15:46 +0800

    1.add hello world.

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import Handler = laya.utils.Handler;
 import Texture = laya.resource.Texture;
 import Loader = laya.net.Loader;
 import Sprite = laya.display.Sprite;
+import display  = laya.display;
 
 class Main extends laya.display.Sprite {
 	constructor() {
@@ -13,6 +14,16 @@ class Main extends laya.display.Sprite {
 	onResDone():void {
 		var pic1: Texture = Loader.getRes("c.png");
 		this.graphics.drawTexture(pic1,0,0);
+
+		var txt:display.Text = new display.Text();
+		txt.text = "Hello Layabox";
+		txt.color = "#FF0000";
+		txt.fontSize = 66;
+		txt.stroke = 5;
+		txt.strokeColor = "#FFFFFF";
+		txt.bold = true;
+		txt.pos(60,100);
+		this.addChild(txt);
 	}
 }
 
